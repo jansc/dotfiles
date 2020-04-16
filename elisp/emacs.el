9f065494848ccb670badfeb1c5af28fed700dc19
Jan Schreiber <jans@ravn.no>
2020-04-16 07:36:38 +0200

    Initial config for emacs-projectile

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -125,7 +125,7 @@
 ;;=====================================================================
 ;; Automatically install packages defined in 'package-list
 ;; Temporarily removed mu4e-maildirs-extension and magit
-(setq package-list '(use-package ace-jump-mode ac-php erc evil ledger-mode org-present php-mode php-extras tramp twittering-mode ido rust-mode tide company elfeed one-themes slime slime-company paredit treemacs treemacs-evil treemacs-icons-dired ox-hugo org-caldav))
+(setq package-list '(use-package ace-jump-mode ac-php erc evil ledger-mode org-present php-mode php-extras tramp twittering-mode ido rust-mode tide company elfeed one-themes slime slime-company paredit treemacs treemacs-evil treemacs-icons-dired ox-hugo org-caldav projectile))
 ;; TODO: After install on fedora 26, the following packages where missing:
 ;; mu4e-maildirs-extension and which-key
 
