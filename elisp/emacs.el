9debb1bc1084a1fcabcb81ff0ab9da0cae20cddf
Jan Schreiber <jans@ravn.no>
2020-08-09 05:45:00 +0200

    Updated melpa URL

--- elisp/emacs.el
+++ elisp/emacs.el
@@ -38,7 +38,7 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/") t)
   (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
   (package-initialize)
