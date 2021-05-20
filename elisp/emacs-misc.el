93c0b0661d3fa3d03cdbf57eb7e08adb22d3d2de
Jan Schreiber <jans@ravn.no>
2021-05-20 20:35:10 +0200

    Added some common lisp configuration

--- elisp/emacs-misc.el
+++ elisp/emacs-misc.el
@@ -17,6 +17,9 @@
 (setq c-basic-offset 2)
 (setq c-default-style "bsd" c-basic-offset 2)
 
+;; We want newlines at end of file
+(setq require-final-newline 1)
+
 (setq confirm-kill-emacs 'y-or-n-p)
 
 (setq history-length 1000)
@@ -29,7 +32,34 @@
 ;; Shortcut for switching between header and implementation files in C/C++
 (global-set-key (kbd "C-x C-o") 'ff-find-other-file)
 
-(setq inferior-lisp-program "/usr/bin/sbcl")
-(slime-setup '(slime-company))
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
+(use-package slime
+  :bind (("M-TAB" . company-complete)
+	 ("C-c C-d C-s" . slime-describe-symbol)
+	 ("C-c C-d C-f" . slime-describe-function))
+  :init
+  (setq slime-lisp-implementations '((sbcl ("sbcl")))
+	slime-default-lisp 'sbcl)
+  (setq common-lisp-hyperspec-root
+        "/usr/local/share/doc/clisp-hyperspec/HyperSpec/")
+
+  ;; (setq common-lisp-hyperspec-symbol-table
+  ;;      (concat common-lisp-hyperspec-root "Data/Map_Sym.txt"))
+  ;; (setq common-lisp-hyperspec-issuex-table
+  ;;      (concat common-lisp-hyperspec-root "Data/Map_IssX.txt"))
+
+  (slime-setup '(slime-fancy slime-company slime-cl-indent)))
+
+
+(load (expand-file-name "/usr/home/jans/quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/local/bin/sbcl")
+
+(show-paren-mode 1)
+(save-place-mode 1)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)))
+
 (provide 'emacs-misc)
